f37c65aed4569a8b9b9ce9fb8decdbf74325323c
Christopher Ariza <ariza@flexatone.com>
2023-06-02 16:57:09 -0700

    add tasks

---
+++ tasks.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import typing as tp
+
+import invoke
+
+
+
+@invoke.task
+def clean(context):
+    '''Clean doc and build artifacts
+    '''
+    context.run('rm -rf coverage.xml')
+    context.run('rm -rf htmlcov')
+    context.run('rm -rf doc/build')
+    context.run('rm -rf build')
+    context.run('rm -rf dist')
+    context.run('rm -rf *.egg-info')
+    context.run('rm -rf .coverage')
+    context.run('rm -rf .mypy_cache')
+    context.run('rm -rf .pytest_cache')
+    context.run('rm -rf .hypothesis')
+    context.run('rm -rf .ipynb_checkpoints')
+
+
+@invoke.task(pre=(clean,))
+def build(context):
+    '''Build packages
+    '''
+    context.run(f'{sys.executable} -m build')
+
+
