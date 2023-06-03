58f6a235ff295be823f6410bf058d024771a3107
Christopher Ariza <ariza@flexatone.com>
2023-06-03 15:28:52 -0700

    0.1.2 RC 1

--- static_frame_pyodide/__init__.py
+++ static_frame_pyodide/__init__.py
@@ -1,23 +1,22 @@
 import sys
+import asyncio
 
 _SF = '1.4.5'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 if sys.platform != 'emscripten':
     raise RuntimeError('This package is only for use in emscripten environments')
 
 _URL = 'https://flexatone.s3.us-west-1.amazonaws.com/packages/'
 
-async def load():
+async def _load():
     import micropip
     await micropip.install('sqlite3',
     f'{_URL}arraymap-0.1.9-cp311-cp311-emscripten_3_1_32_wasm32.whl',
     f'{_URL}arraykit-0.4.8-cp311-cp311-emscripten_3_1_32_wasm32.whl')
-
     await micropip.install('static-frame==1.4.5')
 
-
-load()
+asyncio.run(_load())
 # delegate interface
 from static_frame import *
 
