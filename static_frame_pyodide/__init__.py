5438282455954f8537790177e9cb09bae676245f
Christopher Ariza <ariza@flexatone.com>
2023-06-07 14:47:27 -0700

    additional notes

--- static_frame_pyodide/__init__.py
+++ static_frame_pyodide/__init__.py
@@ -62,10 +62,12 @@ except RuntimeError:
 if loop and loop.is_running():
     print('loop already running')
 
-    loop.create_task(asyncio.wait_for(_micropip_and_import(), None))
+    # loop.create_task(asyncio.wait_for(_micropip_and_import(), None))
 
     # new_loop = asyncio.new_event_loop()
-    # new_loop.run_until_complete(_micropip_and_import())
+
+    # cannot do this as loop is running
+    # loop.run_until_complete(_micropip_and_import())
 
     # loop.create_task(_schedule_and_await(loop))
 
